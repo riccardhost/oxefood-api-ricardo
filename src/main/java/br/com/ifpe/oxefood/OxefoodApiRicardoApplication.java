e4f7cea0fc9c18989c91a5d1980af7f2dbb01f41
aluno <aluno@ifpe>
2024-10-10 21:25:40 -0300

    Primeiros arquivos do Projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiRicardoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiRicardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiRicardoApplication.class, args);
+	}
+
+}
